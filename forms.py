d42f058cba2dbd162c061d78fa99e478ea9fd560
Ryjen <ryjen.pears@gmail.com>
2021-10-10 17:53:19 +0200

    register form added to forms

---
+++ forms.py
@@ -0,0 +1,14 @@
+"""Forms for the Flask Feedback App."""
+from wtforms import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import InputRequired, Length, Email
+
+
+class RegisterForm (FlaskForm):
+    """Form for user registration."""
+
+    username = StringField('Username', validators=[InputRequired(), Length(min=1, max=20)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=6, max=60)])
+    email = StringField('Email', validators=[InputRequired(), Email(), Length(max=50)])
+    first_name = StringField('First Name', validators=[InputRequired(), Length(max=30)])
+    last_name = StringField('Last Name', validators=[InputRequired(), Length(max=30)])
