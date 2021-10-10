e01328864db7f93688a3004a74b2b60aeb9aba8e
Ryjen <ryjen.pears@gmail.com>
2021-10-10 18:02:01 +0200

    added register class method

--- models.py
+++ models.py
@@ -1,7 +1,9 @@
 """Models for Flask-Feedback App."""
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
+bcrypt = Bcrypt()
 
 
 def connect_db(app):
@@ -19,3 +21,21 @@ class User (db.Model):
     email = db.Column(db.String(50), nullable=False)
     first_name = db.Column(db.String(30), nullable=False)
     last_name = db.Column(db.String(30), nullable=False)
+
+    @classmethod
+    def register(cls, username, password, email, first_name, last_name):
+        """Register a new user, and hash their password."""
+
+        hashed = bcrypt.generate_password_hash(password)
+        hashed_utf8 = hashed.decode("utf8")
+
+        user = cls(
+            username=username,
+            password=hashed_utf8,
+            email=email,
+            first_name=first_name,
+            last_name=last_name
+        )
+
+        db.session.add(user)
+        return user
