1892acbfd18cbde21fe5577a4c3529fe8cfbd289
Ryjen <ryjen.pears@gmail.com>
2021-10-10 20:38:19 +0200

    login form added

--- forms.py
+++ forms.py
@@ -12,3 +12,10 @@ class RegisterForm (FlaskForm):
     email = StringField('Email', validators=[InputRequired(), Email(), Length(max=50)])
     first_name = StringField('First Name', validators=[InputRequired(), Length(max=30)])
     last_name = StringField('Last Name', validators=[InputRequired(), Length(max=30)])
+
+
+class LoginForm(FlaskForm):
+    """Login Form."""
+
+    username = StringField('Username', validators=[InputRequired(), Length(min=1, max=20)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=6, max=60)])
