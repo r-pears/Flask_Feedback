e237ba044262cbf29757c2ab46fa8a03ea258bf0
Ryjen <ryjen.pears@gmail.com>
2021-10-10 17:41:38 +0200

    user model added

--- models.py
+++ models.py
@@ -8,3 +8,14 @@ def connect_db(app):
     """Connect this database to the provided Flask App."""
     db.app = app
     db.init_app(app)
+
+
+class User (db.Model):
+    """Model for a site user."""
+    __tablename__ = 'users'
+
+    username = db.Column(db.String(20), nullable=False, unique=True, primary_key=True)
+    password = db.Column(db.Text, nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    first_name = db.Column(db.String(30), nullable=False)
+    last_name = db.Column(db.String(30), nullable=False)
